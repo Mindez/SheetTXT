40036e809adcbb58683267ef5330246b0e9f2490
Chris Atkin <atkinchris@gmail.com>
2021-07-18 10:03:21 +0100

    Batch load cell data to reduce cell-by-cell calls to Google APIs
    This introduces batch loading for cells, to reduce the number of cell-by-cell calls to the Google APIs.
    
    This also introduces strong typing for `SheetTxt.sheets` and removes an errant `await` on a `writeFileSync` call.

--- src/SheetTxt.ts
+++ src/SheetTxt.ts
@@ -6,7 +6,7 @@ import { writeFileSync } from 'fs'
 const REFRESH_INTERVAL = 10
 
 export class SheetTxt {
-  private static sheets = {}
+  private static sheets: Record<string, GoogleSheet> = {}
 
   public static async run (): Promise<void> {
     await this.init()
@@ -30,10 +30,15 @@ export class SheetTxt {
   private static async refreshAll (): Promise<void> {
     await Promise.all(params.map(async sheetParams => {
       const selectedSheet = sheetParams.sheetName
-      await Promise.all(sheetParams.cells.map(async cellParams => {
-        const cellContents = await this.sheets[sheetParams.spreadsheetId].getSingleCell(selectedSheet, cellParams.cell)
-        await writeFileSync(cellParams.path, cellContents)
-      }))
+
+      const cellsRefs = sheetParams.cells.map(({ cell }) => cell)
+      const cellsData = await this.sheets[sheetParams.spreadsheetId].getCells(selectedSheet, ...cellsRefs)
+
+      sheetParams.cells.forEach((cellParams, cellIndex) => {
+        const cellContents = cellsData[cellIndex]
+
+        writeFileSync(cellParams.path, cellContents)
+      })
     }))
   }
 }
